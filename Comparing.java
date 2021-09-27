bb0185a10628923e8ccc21b9499c1c1d9bfbf320
sunil prajapati <91301312+sunilmother@users.noreply.github.com>
2021-09-27 21:03:05 +0530

    Create Comparing.java

---
+++ Comparing.java
@@ -0,0 +1,17 @@
+public class Comparing{
+	public static void main(String[] args){
+		int a= 50;
+		int b=4;
+		int c=52;
+		
+		if(a>b && a>c){
+			System.out.print(a+" is the greatest number");
+		}
+		else if(b>a && b>c){
+			System.out.print(b+" is the greatest number");
+		}
+		else{
+			System.out.print(c+" is the greatest number");
+		}
+	}
+}
