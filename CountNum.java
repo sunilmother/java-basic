bcd6ef40a83a4ad696c520c3e67f86d4cb7c433f
sunil prajapati <91301312+sunilmother@users.noreply.github.com>
2021-09-27 20:59:30 +0530

    Create CountNum.java

---
+++ CountNum.java
@@ -0,0 +1,16 @@
+public class CountNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 45938, count=0;
+		for (int i = n; i>0; i=i/10 ) {
+			count++;
+			
+			
+		}
+		System.out.println("The number has " +count+" digits");
+
+	}
+
+}
+© 2021 GitHub
