f888d48200b948711988e747758779a1a3082ceb
sunil prajapati <91301312+sunilmother@users.noreply.github.com>
2021-09-27 21:03:45 +0530

    Create sumnum.java

---
+++ sumnum.java
@@ -0,0 +1,11 @@
+public class sumofnum {
+
+	public static void main(String[] args) {
+		int i , num = 10, sum = 0;
+		for (i=1; i <= num; i++) {
+			sum = sum + i;
+		}
+        System.out.println("Sum is "+ sum);
+	}
+
+}
